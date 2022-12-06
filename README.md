c69ef2e3e97925f02d20bad9d1fa93fc4d7c43c0
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-12-06 01:07:46 -0800

    project init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Astro Blog
+Tutorial from [Astro Docs](https://docs.astro.build/en/tutorial/0-introduction/)
+
+Astro Docs on [Deploying to GitHub](https://docs.astro.build/en/guides/deploy/github/)
---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://gesty.dev',
+  // commented out because using a custom domain instead of 'name.github.io'
+  //base: '/astroBlog',
+});

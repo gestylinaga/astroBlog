c959ead6d68e1a4bd48f1c51988b711ba3bb1ba0
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-12-09 23:58:00 -0800

    added base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gesty.dev',
-  // commented out because using a custom domain instead of 'name.github.io'
-  //base: '/astroBlog',
+  base: '/astroBlog',
 });
